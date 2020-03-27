dae51b988f3df200fb0255e73a451265e14f1f2c
Anton Veldanov <anton.veldanov@gmail.com>
2020-03-26 20:31:02 -0700

    iOS

--- DoItRight/ToDoListViewController.swift
+++ DoItRight/ToDoListViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
+  let itemArray = ["TODO1", "TODO2", "TODO3"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
-  }
 
+
+  }
+//MARK: - TableView DataSource Methods
+  
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemArray.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+// indexPath contains SectionNumber and RowNumber
+    print(indexPath.row)
+    cell.textLabel?.text = itemArray[indexPath.row]
+    return cell
+  }
+  
+  
 
 }
 
